2ca6135d27c3c0b2fc31cdaac0eb5e10270716c5
fozorri <164972782+fozorri@users.noreply.github.com>
2024-03-25 23:24:46 -0400

    Add files via upload

---
+++ modules/feeder/rss5de6.php
@@ -0,0 +1,77 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<rss version="2.0">
+	<channel>
+		<title><![CDATA[Innova, Expert Inova Networks SRL: Distribuidor autorizado en Bolivia de las marcas: Mikrotik, Ubiquiti, Hikvision, Grandstream, TrendNet]]></title>
+		<link>http://inova.com.bo/</link>
+		<mail>erojas@inova.com.bo</mail>
+		<generator>PrestaShop</generator>
+		<language>es</language>
+		<image>
+			<title><![CDATA[Innova, Expert Inova Networks SRL: Distribuidor autorizado en Bolivia de las marcas: Mikrotik, Ubiquiti, Hikvision, Grandstream, TrendNet]]></title>
+			<url>http://inova.com.bo/img/logo.jpg</url>
+			<link>http://inova.com.bo/</link>
+		</image>
+		<item>
+			<title><![CDATA[Camara Bala Varifocal 1.3MP - 57.22 USD ]]></title>
+			<description><![CDATA[<img src='http://inova.com.bo/img/p/3/9/0/390-small_default.jpg' title='Camara Bala Varifocal 1.3MP' alt='thumb' /><div class="page" title="Page 5">
+<div class="layoutArea">
+<div class="column">
+<p style="text-align: left;"><span><span>Camara tipo bala varifocal turboHD para exterior metalica </span>1,3MP (1280X720) infrarojo 40 metros, IP66, </span><span style="text-align: left;">Resolución: HD720 • NTSC: 1280 (H) x 720 (V) • Lente: 3.6mm • Angulo de Visión: 68.4° Iluminación: 0.1Lux@F1.2/ 0Lux IR On • Velocidad de Obturador: 1/60 a 1/100,000 • BLC Alimentación: 12Vdc +/- 10% • Consumo: 5 Watt. No incluye fuente.</span></p>
+</div>
+</div>
+</div>]]></description>
+			<link><![CDATA[http://inova.com.bo/-hikvision-analogicas/47-camara-varifocal-tipo-bala-exterior.html]]></link>
+		</item>
+		<item>
+			<title><![CDATA[Camara Bala 2MP - 27.59 USD ]]></title>
+			<description><![CDATA[<img src='http://inova.com.bo/img/p/3/9/2/392-small_default.jpg' title='Camara Bala 2MP' alt='thumb' /><p style="text-align: left;"><span style="text-align: left;"><span>Camara de vigilancia tipo bala 2MP FullHD </span>1080P, infrarojo 10 a 20M , proteccion  IP66</span>, Lente: 2,8mm • Angulo de Vision: 99.72° • DNR, Alimentación: 12Vdc • Consumo de energia 3.5 Watt. No incluye fuente. </p>]]></description>
+			<link><![CDATA[http://inova.com.bo/camaras-de-seguridad-bolivia/49-ds-2ce16d1t-ir.html]]></link>
+		</item>
+		<item>
+			<title><![CDATA[Camara tipo Bala 2MP - 83.05 USD ]]></title>
+			<description><![CDATA[<img src='http://inova.com.bo/img/p/3/9/3/393-small_default.jpg' title='Camara tipo Bala 2MP' alt='thumb' /><div class="page" title="Page 5">
+<div class="layoutArea">
+<div class="column">
+<p style="text-align: left;"><span><span>Camara de seguridad tipo bala 2MP </span>(1920 X 1080), infrarojo extra largo 30 a 40M, proteccion IP66, </span>Resolución: HD1080P • NTSC: 1920 (H) x 1080 (V) • Lente: 3.6mm • Angulo de Visión: 90.9° Alimentación: 12Vdc • Consumo: 5 Watt. Incluye Soporte, no incluye fuente. </p>
+</div>
+</div>
+</div>]]></description>
+			<link><![CDATA[http://inova.com.bo/camaras-de-seguridad-bolivia/50-ds-2ce16d1t-it3-hikvision-bolivia-camaras-de-seguridad.html]]></link>
+		</item>
+		<item>
+			<title><![CDATA[Camara tipo Bala 1MP - 128.30 USD ]]></title>
+			<description><![CDATA[<img src='http://inova.com.bo/img/p/3/9/7/397-small_default.jpg' title='Camara tipo Bala 1MP' alt='thumb' /><p>Camara IP tipo bala 1MP, 720P, soporta PoE, infrarojo 20 metros, compresion H264, uso en exterior.</p>]]></description>
+			<link><![CDATA[http://inova.com.bo/camaras-de-seguridad-bolivia/261-ds-2cd2010f-i-hikvision-bolivia-la-paz-santa-cruz.html]]></link>
+		</item>
+		<item>
+			<title><![CDATA[Camara PTZ 2MP - 765.23 USD ]]></title>
+			<description><![CDATA[<img src='http://inova.com.bo/img/p/4/0/7/407-small_default.jpg' title='Camara PTZ 2MP' alt='thumb' /><p style="text-align: left;"><span>Camara de vigilancia rotatoria, domo PTZ TurboHD 1080p 2MP,  23X zoom óptico, dWDR e IR inteligente EXIR polarizado para 150m.</span></p>]]></description>
+			<link><![CDATA[http://inova.com.bo/camaras-de-seguridad-bolivia/271-ds-2ae5223ti-camaras-de-seguridad-bolivia-ptz-domo.html]]></link>
+		</item>
+		<item>
+			<title><![CDATA[DISCO DURO DE 1TB PURPURA - 96.98 USD ]]></title>
+			<description><![CDATA[<img src='http://inova.com.bo/img/p/4/1/7/417-small_default.jpg' title='DISCO DURO DE 1TB PURPURA' alt='thumb' /><p style="text-align: left;"><span>Disco duro SATA 1TB serie WD PURPLE optimizado para CCTV, 5400RPM, 24/7</span></p>]]></description>
+			<link><![CDATA[http://inova.com.bo/camaras-de-seguridad-bolivia/281-wd10-purx.html]]></link>
+		</item>
+		<item>
+			<title><![CDATA[Joystick para CCTV - 482.61 USD ]]></title>
+			<description><![CDATA[<img src='http://inova.com.bo/img/p/4/2/2/422-small_default.jpg' title='Joystick para CCTV' alt='thumb' /><p style="text-align: left;"><span>JOYSTICK USB para movimiento de cámaras PTZ compatible con software iVMS4200 (v2.02.03 o superior) camaras Hikvision o EPCOM<br /></span></p>]]></description>
+			<link><![CDATA[http://inova.com.bo/camaras-de-seguridad-bolivia/283-xk-05-joystick-ptz-camaras-de-seguridad.html]]></link>
+		</item>
+		<item>
+			<title><![CDATA[Camara 1.3 bullet VF - 265.23 USD ]]></title>
+			<description><![CDATA[<img src='http://inova.com.bo/img/p/8/9/4/894-small_default.jpg' title='Camara 1.3 bullet VF' alt='thumb' /><p>Camara IP, 1.3MP,  VF IR Bullet Camera  <br /> 1.3 megapixel alta resolucion<br /> 2.8-12mm vari-focal lens<br /> 3D DNR &amp; DWDR &amp; BLC<br /> IP67 rating<br /> IR range: 20m to 30m<br /> Tia / Noche</p>]]></description>
+			<link><![CDATA[http://inova.com.bo/camaras-de-seguridad-bolivia/694-camara-13-bullet-vf.html]]></link>
+		</item>
+		<item>
+			<title><![CDATA[NVR 4 canales - 92.24 USD ]]></title>
+			<description><![CDATA[<img src='http://inova.com.bo/img/p/8/9/5/895-small_default.jpg' title='NVR 4 canales' alt='thumb' /><p>NVR de 4 canales <br /> Grabación de resolución de hasta 2 megapíxeles<br /> Salida HDMI y VGA hasta una resolución de 1920 * 1080P<br /><br /></p>]]></description>
+			<link><![CDATA[http://inova.com.bo/camaras-de-seguridad-bolivia/696-nvr-4-canales.html]]></link>
+		</item>
+		<item>
+			<title><![CDATA[Camara IP bala 2MP - 111.93 USD ]]></title>
+			<description><![CDATA[<img src='http://inova.com.bo/img/p/8/9/7/897-small_default.jpg' title='Camara IP bala 2MP' alt='thumb' /><p>Mini bala IP 2MP con IR inteligente para 30m, ONVIF, Hik-Connect P2P, dWDR, Almacenamiento MicroSD</p>]]></description>
+			<link><![CDATA[http://inova.com.bo/camaras-de-seguridad-bolivia/698-camara-ip-bala-2mp.html]]></link>
+		</item>
+	</channel>
+</rss>
